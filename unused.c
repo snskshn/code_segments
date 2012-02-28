bbc052d225d8c042e30af619cfc9b74460e7e4d5
Seokshin Son <snskshn@gmail.com>
2012-02-28 15:50:41 +0900

    [gcc] how to eliminate unused parameter was explaind.

---
+++ unused.c
@@ -0,0 +1,13 @@
+#ifdef UNUSED
+#elif defined(__GNUC__)
+# define UNUSED(x) UNUSED_ ## x __attribute__((unused)) 
+#elif defined(__LCLINT__)
+# define UNUSED(x) /*@unused@*/ x
+#else
+# define UNUSED(x) x
+#endif
+
+int main(int UNUSED(argc), char **UNUSED(argv))
+{
+    return 0;
+}
