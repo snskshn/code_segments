a91493b79f250b4f588464ef9cd8789432bed32a
Seokshin Son <snskshn@gmail.com>
2012-03-02 11:37:12 +0900

    [libgc] standard time format has added.

--- strftime.c
+++ strftime.c
@@ -10,5 +10,9 @@ int main()
     strftime(format, 128, "%Y %B %d %A %l:%M:%S %p", localtime(&t));
     puts(format);
 
+    // yyyy-MM-dd HH:mm:ssZ
+    strftime(format, 128, "%Y-%m-%d %H:%M:%S%Z\n", localtime(&t));
+    puts(format);
+
     return 0;
 }
