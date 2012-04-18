7902a2c766d2431e5163fb31bc2619ca8c0f1a31
Seokshin Son <snskshn@gmail.com>
2012-04-18 15:51:36 +0900

    [system] system() = fork() + execve()

--- system.c
+++ system.c
@@ -1,8 +1,33 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int run_command(char *const argv[])
+{
+	int pid = fork();
+	char *env[] = {NULL};
+
+	assert(pid >= 0);
+
+	if (pid == 0) {
+		execve(argv[0], argv, env);
+	}
+
+	return pid;
+}
 
 int main()
 {
+#if 0
     system("ls -al");
+#else
+    char *const argv[] = {"/bin/ls", "-al", NULL};
+    int pid = run_command(argv);
+    waitpid(pid, NULL, 0);
+#endif
 
     return 0;
 }
