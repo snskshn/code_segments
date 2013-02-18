ecfb0ffbed434e3a2d2d842e2206a2e1fdfd854c
Seokshin Son <snskshn@gmail.com>
2013-02-18 11:36:41 +0900

    [mysql] First example
    http://zetcode.com/tutorials/mysqlcapitutorial/

---
+++ mysql/version.c
@@ -0,0 +1,10 @@
+#include <my_global.h>
+#include <mysql.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("MySQL client version: %s\n", mysql_get_client_info());
+
+	return 0;
+}
